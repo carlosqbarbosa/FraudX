3a06686b98a7e421e84f833ff47b39b8f1657f9b
Carlosbarbosa <carloshenrique29.qb@gmail.com>
2025-05-10 21:08:18 -0300

    começo da página princiapl

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('fraudForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Formulário enviado com sucesso! (Aqui você chamaria a API ou modelo de detecção)');
+  });
+  
