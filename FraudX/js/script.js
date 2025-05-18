76c8f6a276e39d989dabce31821dfa17c8c722c9
Rayanne Sales <rayannesales2026@gmail.com>
2025-05-18 05:56:55 +0000

    Reorganizei estrutura de pastas e atualizei o conteudo do projeto

--- FraudX/js/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('fraudForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Formulário enviado com sucesso! (Aqui você chamaria a API ou modelo de detecção)');
-  });
-  
